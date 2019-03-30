dcc72a7bd76c0acc984c7502e529ea30fdf0df91
fareedat <phariedah19@gmail.com>
2019-03-30 17:11:15 +0100

    Added view.js

---
+++ view.js
@@ -0,0 +1,1 @@
+/*  my view so far on ES6. it is very concise and makes your life easier. I just started integrating it into my work and I love with very much. My brain default is still ES% thou..lol but with time it will change. By the way, I created this file to practice something, so don't thin I'm weird. OK Bye!*/
