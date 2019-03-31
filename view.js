eae0fd622a74dd005b45b6ecabe2f8c5116d4ccf
rheehar <phariedah19@gmail.com>
2019-03-31 18:39:46 +0100

    Edited view.js

--- view.js
+++ view.js
@@ -1,1 +1,5 @@
-/*  my view so far on ES6. it is very concise and makes your life easier. I just started integrating it into my work and I love with very much. My brain default is still ES% thou..lol but with time it will change. By the way, I created this file to practice something, so don't thin I'm weird. OK Bye!*/
+/*  my view so far on ES6. it is very concise and makes your life easier. I just started integrating it into my work and I love with very much. My brain default is still ES% thou..lol but with time it will change. 
+By the way, I created this file to practice something, so don't thin I'm weird. OK Bye!
+
+I am building an interesting project. Watch my new forkify repository. cheers
+*/
